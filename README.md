890c69ae05519627169944fa009e8e6271843e3a
gareyyy <gk10guce@gmail.com>
2026-08-21 20:54:09 +1000

    began work on population, not sure what happened to the rest of these guys

--- README.md
+++ README.md
@@ -4,18 +4,4 @@ training LLMs with evolution and that biology stuff from high school
 base LLM code ~~stolen~~ referenced from [nanochat](https://github.com/karpathy/nanochat)
 ### Setup
 
-evolutionGPT uses [uv](https://docs.astral.sh/uv/) for dependency management. To install:
-
-```bash
-uv sync --extra gpu    # Use for CUDA (A100/H100/etc.)
-uv sync --extra cpu    # (or) Use for CPU-only / MPS
-source .venv/bin/activate
-```
-
-For development (adds pytest, matplotlib, ipykernel, transformers, etc.):
-
-```bash
-uv sync --extra gpu --group dev
-```
-
-
+evolutionGPT uses [uv](https://docs.astral.sh/uv/) for dependency management.
---
+++ build/lib/evolution/population.py
@@ -0,0 +1,27 @@
+import torch
+from nanochat.gpt import GPTConfig, GPT
+
+def initialise_model(config: GPTConfig, device):
+    # Build the model, move to device, init the weights
+    with torch.device("meta"):
+        model = GPT(config)
+    model.to_empty(device=device) # 2) All tensors get storage on target device but with uninitialized (garbage) data
+    model.init_weights() # 3) All tensors get initialized
+    return model
+
+class Population:
+    def __init__(self, num_strongest, gptconfig: GPTConfig, device) -> None:
+        self.num_strongest = num_strongest
+        self.pop_cap = self.num_strongest**2
+        self.population = []
+        self.gptconfig = gptconfig
+        self.device = device
+        for _ in range(self.num_strongest):
+            model = initialise_model(self.gptconfig, self.device)
+            self.population.append(model)
+
+    def fill_with_random(self):
+        assert len(self.population) == self.num_strongest
+        for _ in range(self.pop_cap - self.num_strongest):
+            model = initialise_model(self.gptconfig, self.device)
+            self.population.append(model)
